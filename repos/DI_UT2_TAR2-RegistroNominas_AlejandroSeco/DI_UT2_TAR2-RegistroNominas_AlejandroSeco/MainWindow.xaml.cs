dd77887b4394052b8295902ebca2866d2d5fd588
alejandrosp16 <alejandrosecopineda@gmail.com>
2022-12-04 22:44:39 +0100

    04/12/22

---
+++ repos/DI_UT2_TAR2-RegistroNominas_AlejandroSeco/DI_UT2_TAR2-RegistroNominas_AlejandroSeco/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DI_UT2_TAR2_RegistroNominas_AlejandroSeco
+{
+    public partial class MainWindow : Window
+    {
+        Puesto puesto;
+        int aniosExperiencia;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            llenarCmbHijos();
+        }
+
+        private void calcularNomina_Click(object sender, RoutedEventArgs e)
+        {
+            Trabajador t = obtenerTrabajadorForm();
+            if(t != null)
+            {
+                MessageBox.Show("El sueldo del trabajador es -> " + t.sueldo + "\n");
+            }
+        }
+
+        private Trabajador obtenerTrabajadorForm()
+        {
+            double sueldo = 0;
+            switch (puesto)
+            {
+                case Puesto.OBRERO:
+                    sueldo = 800;
+                    break;
+                case Puesto.ADMINISTRATIVO:
+                    sueldo = 900;
+                    break;
+                case Puesto.TECNICO:
+                    sueldo = 1000;
+                    break;
+                case Puesto.PROFESIONAL:
+                    sueldo = 1200;
+                    break;
+                default:
+                    MessageBox.Show("ERROR");
+                    break;
+            }
+
+            bool afiliacionSindical = false;
+
+            if ((bool)checkAfiliado.IsChecked)
+            {
+                sueldo += 50;
+                afiliacionSindical = true;
+            }
+
+            switch (aniosExperiencia)
+            {
+                case 0:
+                    break;
+                case 1:
+                    sueldo += 100;
+                    break;
+                case 2:
+                    sueldo += 200;
+                    break;
+                default:
+                    MessageBox.Show("ERROR");
+                    break;
+            }
+
+            int numHijos = Convert.ToInt32(cmbHijos.SelectedItem);
+
+            sueldo += numHijos * 100;
+
+            string nombre = txtNombre.Text;
+            string apellido1 = txtApellido1.Text;
+            string apellido2 = txtApellido2.Text;
+            string edad = txtEdad.Text;
+            string dni = txtDni.Text;
+
+            if (!(string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido1) || string.IsNullOrEmpty(apellido2) || string.IsNullOrEmpty(edad) || string.IsNullOrEmpty(dni)))
+            {
+                return new Trabajador(nombre, apellido1, apellido2, Convert.ToInt32(edad), numHijos, dni, afiliacionSindical, aniosExperiencia, puesto, sueldo);
+            } else
+            {
+                MessageBox.Show("Por favor rellene los campos");
+                return null;
+            }
+        }
+
+        private void radioObrero_Checked(object sender, RoutedEventArgs e)
+        {
+            puesto = Puesto.OBRERO;
+        }
+
+        private void radioAdministrativo_Checked(object sender, RoutedEventArgs e)
+        {
+            puesto = Puesto.ADMINISTRATIVO;
+        }
+
+        private void radioProfesional_Checked(object sender, RoutedEventArgs e)
+        {
+            puesto = Puesto.PROFESIONAL;
+        }
+
+        private void radioTecnico_Checked(object sender, RoutedEventArgs e)
+        {
+            puesto = Puesto.TECNICO;
+        }
+
+        private void radioMenos5_Checked(object sender, RoutedEventArgs e)
+        {
+            aniosExperiencia = 0;
+        }
+
+        private void radioEntre5y10_Checked(object sender, RoutedEventArgs e)
+        {
+            aniosExperiencia = 1;
+        }
+
+        private void radioMas10_Checked(object sender, RoutedEventArgs e)
+        {
+            aniosExperiencia = 2;
+        }
+
+        private void llenarCmbHijos()
+        {
+            List<int> numHijos = new List<int>();
+            for (int i = 0; i <= 20; i++)
+            {
+                numHijos.Add(i);
+            }
+            cmbHijos.ItemsSource = numHijos;
+            cmbHijos.SelectedItem = 0;
+        }
+    }
+}
---
+++ repos/DI_UT2_TAR2-RegistroNominas_AlejandroSeco/DI_UT2_TAR2-RegistroNominas_AlejandroSeco/Trabajador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI_UT2_TAR2_RegistroNominas_AlejandroSeco
+{
+    public class Trabajador
+    {
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+        public int edad { get; set; }
+        public int numeroHijos { get; set; }
+        public string dni { get; set; }
+        public bool afiliacionSindical { get; set; }
+        public int aniosExperiencia { get; set; }
+        public Puesto puesto { get; set; }
+        public double sueldo { get; set; }
+
+        public Trabajador(string nombre, string apellido1, string apellido2, int edad, int numeroHijos, string dni, bool afiliacionSindical, int aniosExperiencia, Puesto puesto, double sueldo)
+        {
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.edad = edad;
+            this.numeroHijos = numeroHijos;
+            this.dni = dni;
+            this.afiliacionSindical = afiliacionSindical;
+            this.aniosExperiencia = aniosExperiencia;
+            this.puesto = puesto;
+            this.sueldo = sueldo;
+        }
+    }
+
+    public enum Puesto
+    {
+        OBRERO,
+        ADMINISTRATIVO,
+        TECNICO,
+        PROFESIONAL
+    }
+}
